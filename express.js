b2366d0e57fd93f0bcf7592116a546f9566b079f
guobb <18576469764@163.com>
2017-02-18 22:28:33 +0800

    express get

---
+++ express.js
@@ -0,0 +1,28 @@
+/**
+ * Created by apple on 17/2/18.
+ */
+/**
+ * 1. 安装
+ * 2. 使用
+ *
+ */
+// 加载express
+
+const express = require('express');
+
+//获取文件对象
+
+let app = express();
+
+// 配置路由
+// 当用户访问'／'的时候，会执行后面的回调函数
+app.get('/', (req, res) => {
+
+    // send 可以自动判断参数类型,自动转换响应信息
+    // 并且自动设置Content-Type
+    res.send('hello express');
+});
+
+// 启动服务器
+
+app.listen(3000);
